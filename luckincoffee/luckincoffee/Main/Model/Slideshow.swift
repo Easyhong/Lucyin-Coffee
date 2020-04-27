32712f064606d30a5d77dda8f7fa907cc637d37b
Easyhong <2583109301@qq.com>
2020-04-27 23:21:06 +0800

    feat(feature): add group file

---
+++ luckincoffee/luckincoffee/Main/Model/Slideshow.swift
@@ -0,0 +1,17 @@
+//
+//  Slideshow.swift
+//  luckincoffee
+//
+//  Created by taozhang on 2020/4/27.
+//  Copyright © 2020 Easyhong. All rights reserved.
+//
+
+import UIKit
+//
+struct Slideshow {
+    
+    var slideshowImage: UIImage?
+    var isShowLable: Bool?
+    var textLabel: String?
+}
+
---
+++ luckincoffee/luckincoffee/Main/view/MainTableView.swift
@@ -0,0 +1,28 @@
+//
+//  MainTableView.swift
+//  luckincoffee
+//
+//  Created by taozhang on 2020/4/27.
+//  Copyright © 2020 Easyhong. All rights reserved.
+//
+
+import UIKit
+
+class MainTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+    
+    
+
+}
+
+
+
