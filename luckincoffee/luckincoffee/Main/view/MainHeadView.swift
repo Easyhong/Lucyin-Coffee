022680cb26e347d8fc4f64f6e6b85e47ad107e66
Easyhong <2583109301@qq.com>
2020-04-27 22:40:28 +0800

    feat(feature): add file

---
+++ luckincoffee/luckincoffee/Main/view/MainHeadView.swift
@@ -0,0 +1,21 @@
+//
+//  MainHeadView.swift
+//  luckincoffee
+//
+//  Created by taozhang on 2020/4/27.
+//  Copyright © 2020 Easyhong. All rights reserved.
+//
+
+import UIKit
+
+class MainHeadView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
