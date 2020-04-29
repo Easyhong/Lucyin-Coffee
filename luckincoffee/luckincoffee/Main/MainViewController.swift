c27dc8051b34edec22ae7f838f03c07165b7cc6e
Easyhong <2583109301@qq.com>
2020-04-29 21:55:15 +0800

    feat(feature): Add ScrollerView Layout

--- luckincoffee/luckincoffee/Main/MainViewController.swift
+++ luckincoffee/luckincoffee/Main/MainViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  luckincoffee
 //
-//  Created by taozhang on 2020/4/27.
+//  Created by taozhang on 2020/4/29.
 //  Copyright © 2020 Easyhong. All rights reserved.
 //
 
@@ -12,9 +12,19 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
-    
     }
 
-}
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
